84ff3310ccc08dae518742dcb609c21c44e170d2
Sushil Vas <vasemmanuels@gmail.com>
2024-06-30 21:28:53 -0400

    Created angular frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,26 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GetdataComponent } from './getdata/getdata.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
+import { routes } from './app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GetdataComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [provideRouter(routes), 
+    provideClientHydration(),provideHttpClient()],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
